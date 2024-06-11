09699c1de4ffaa7537a70c3b1e7ac5965ffdbde0
hoangdu999 <hoangdu1999s2@gmail.com>
2024-06-11 14:13:52 +0700

    robot

--- src/components/BabylonCpn.vue
+++ src/components/BabylonCpn.vue
@@ -31,7 +31,7 @@ import {
   ActionManager,
   ExecuteCodeAction,
   WebXRHitTest,
-  WebXRBackgroundRemover
+  WebXRBackgroundRemover,
 } from "@babylonjs/core";
 import "@babylonjs/loaders";
 import "@babylonjs/inspector";
@@ -130,26 +130,30 @@ export default {
     },
 
     async loadModel(scene) {
-      const model = new MeshBuilder.CreateBox("box", { width: 0.2, height: 0.2, depth: 0.2 }, scene);
-      model.rotationQuaternion = new Quaternion();
-      model.position.y += 0.1;
+      // Đường dẫn đến tệp robot.glb
+      const modelUrl = "/model/robot.glb";
 
-     // Tạo vật liệu chuẩn và áp dụng cho mô hình
-      const material = new StandardMaterial("boxMaterial", scene);
-      material.diffuseColor = new Color3(1, 0, 0); // Red color
-      model.material = material;
+      SceneLoader.ImportMesh("", "", modelUrl, scene, (meshes) => {
+        const model = meshes[0];
+        model.scaling = new Vector3(0.2, 0.2, 0.2); // Điều chỉnh tỷ lệ nếu cần
+        model.position.y += 0.1;
 
-      this.shadowGenerator.addShadowCaster(model);
-      model.receiveShadows = true;
+        this.shadowGenerator.addShadowCaster(model);
+        model.receiveShadows = true;
 
-      this.model = model;
-      this.marker = model.clone("marker");
-      this.marker.material = material.clone("markerMaterial");
-      this.marker.material.alpha = 0.5; // Làm mờ hộp khi là marker
-      this.marker.isVisible = false;
+        this.model = model;
+        this.marker = model.clone("marker");
+        this.marker.material = model.material.clone("markerMaterial");
+        this.marker.material.alpha = 0.5; // Làm mờ hộp khi là marker
+        this.marker.isVisible = false;
+      });
     },
-    Ground(scene){
-      const ground = MeshBuilder.CreateGround('ground', { width: 4, height: 4 }, scene);
+    Ground(scene) {
+      const ground = MeshBuilder.CreateGround(
+        "ground",
+        { width: 4, height: 4 },
+        scene
+      );
       ground.receiveShadows = true;
 
       // Tạo vật liệu tiêu chuẩn và áp dụng cho mặt đất
@@ -168,7 +172,9 @@ export default {
 
         const fm = xr.baseExperience.featuresManager;
         const xrTest = fm.enableFeature(WebXRHitTest, "latest");
-        const xrBackgroundRemover = fm.enableFeature(WebXRBackgroundRemover.Name);
+        const xrBackgroundRemover = fm.enableFeature(
+          WebXRBackgroundRemover.Name
+        );
 
         xr.baseExperience.sessionManager.onXRSessionInit.add(() => {
           this.model.setEnabled(false);
@@ -178,7 +184,11 @@ export default {
           if (results.length) {
             this.hitTest = results[0];
             this.marker.isVisible = true;
-            this.hitTest.transformationMatrix.decompose(this.marker.scaling, this.marker.rotationQuaternion, this.marker.position);
+            this.hitTest.transformationMatrix.decompose(
+              this.marker.scaling,
+              this.marker.rotationQuaternion,
+              this.marker.position
+            );
           } else {
             this.hitTest = undefined;
             this.marker.isVisible = false;
@@ -207,15 +217,17 @@ export default {
       guiButton.onPointerUpObservable.add(() => {
         if (this.hitTest) {
           let matrix = this.hitTest.transformationMatrix;
-          matrix.decompose(this.model.scaling, this.model.rotationQuaternion, this.model.position);
+          matrix.decompose(
+            this.model.scaling,
+            this.model.rotationQuaternion,
+            this.model.position
+          );
           this.model.setEnabled(true);
           this.model.material.alpha = 1; // Đặt hộp hoàn toàn rõ ràng sau khi đặt
           this.marker.isVisible = false;
         }
       });
-  
     },
-
   },
 };
 </script>
