cf4e41696a844d23464fddcfea8069b383beaf97
hoangdu999 <hoangdu1999s2@gmail.com>
2024-06-25 08:56:02 +0700

    robot 18

--- src/components/BabylonCpn.vue
+++ src/components/BabylonCpn.vue
@@ -31,7 +31,7 @@ import {
   ActionManager,
   ExecuteCodeAction,
   WebXRHitTest,
-  WebXRBackgroundRemover,
+  WebXRBackgroundRemover
 } from "@babylonjs/core";
 import "@babylonjs/loaders";
 import "@babylonjs/inspector";
@@ -130,50 +130,26 @@ export default {
     },
 
     async loadModel(scene) {
-      // const modelUrl = "models/robot.glb";
-      // SceneLoader.ImportMesh("", "", modelUrl, scene, (meshes) => {
-      //   if (meshes.length > 0) {
-      //     const model = meshes[0];
-      //     model.scaling = new Vector3(0.05, 0.05, 0.05);
-      //     model.position.y += 0.1;
-      //     this.shadowGenerator.addShadowCaster(model);
-      //     model.receiveShadows = true;
-      //     model.rotationQuaternion = new Quaternion();
-      //     this.model = model;
-          
-      //     // Tạo marker và áp dụng tất cả các thuộc tính từ mô hình ban đầu
-      //     this.marker = model.clone("marker");
-      //     this.marker.scaling = model.scaling.clone();
-      //     this.marker.position = model.position.clone();
-      //     this.marker.rotationQuaternion = model.rotationQuaternion.clone();
-      //     this.marker.material = model.material.clone("markerMaterial");
-      //     this.marker.material.alpha = 0.5;
-      //     this.marker.isVisible = false;
-      //   } else {
-      //     console.error("Failed to load the model");
-      //   }
-      // });
       const model = new MeshBuilder.CreateBox("box", { width: 0.2, height: 0.2, depth: 0.2 }, scene);
       model.rotationQuaternion = new Quaternion();
       model.position.y += 0.1;
+
      // Tạo vật liệu chuẩn và áp dụng cho mô hình
       const material = new StandardMaterial("boxMaterial", scene);
       material.diffuseColor = new Color3(1, 0, 0); // Red color
       model.material = material;
+
       this.shadowGenerator.addShadowCaster(model);
       model.receiveShadows = true;
+
       this.model = model;
       this.marker = model.clone("marker");
       this.marker.material = material.clone("markerMaterial");
       this.marker.material.alpha = 0.5; // Làm mờ hộp khi là marker
       this.marker.isVisible = false;
     },
-    Ground(scene) {
-      const ground = MeshBuilder.CreateGround(
-        "ground",
-        { width: 4, height: 4 },
-        scene
-      );
+    Ground(scene){
+      const ground = MeshBuilder.CreateGround('ground', { width: 4, height: 4 }, scene);
       ground.receiveShadows = true;
 
       // Tạo vật liệu tiêu chuẩn và áp dụng cho mặt đất
@@ -192,9 +168,7 @@ export default {
 
         const fm = xr.baseExperience.featuresManager;
         const xrTest = fm.enableFeature(WebXRHitTest, "latest");
-        const xrBackgroundRemover = fm.enableFeature(
-          WebXRBackgroundRemover.Name
-        );
+        const xrBackgroundRemover = fm.enableFeature(WebXRBackgroundRemover.Name);
 
         xr.baseExperience.sessionManager.onXRSessionInit.add(() => {
           this.model.setEnabled(false);
@@ -204,11 +178,7 @@ export default {
           if (results.length) {
             this.hitTest = results[0];
             this.marker.isVisible = true;
-            this.hitTest.transformationMatrix.decompose(
-            //  this.marker.scaling,
-              this.marker.rotationQuaternion,
-              this.marker.position
-            );
+            this.hitTest.transformationMatrix.decompose(this.marker.scaling, this.marker.rotationQuaternion, this.marker.position);
           } else {
             this.hitTest = undefined;
             this.marker.isVisible = false;
@@ -243,7 +213,9 @@ export default {
           this.marker.isVisible = false;
         }
       });
+  
     },
+
   },
 };
 </script>
