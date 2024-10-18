b752944125c2b7439ef3aa6fbda388e97c40e95c
hoangdu999 <hoangdu1999s2@gmail.com>
2024-10-18 22:33:31 +0700

    create pjTruong 8

--- vite.config.js
+++ vite.config.js
@@ -13,13 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]'
-      }
-    }
-  }
+  
 })
