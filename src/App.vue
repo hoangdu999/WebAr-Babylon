15843f9dd258649cee9f1ba98ff22ae089879ed5
hoangdu999 <hoangdu1999s2@gmail.com>
2024-10-18 22:48:08 +0700

    create pjTruong 11

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
-<script setup>
-import BabylonScene from './components/BabylonScene.vue';
-</script>
-
 <template>
   <div id="app">
     <BabylonScene />
   </div>
 </template>
+<script>
+  import BabylonScene from './components/BabylonScene.vue';
+  export default {
+  name: 'App',
+  components: {
+    BabylonScene
+  }
+}
+</script>
+<style>
+</style>
+  
